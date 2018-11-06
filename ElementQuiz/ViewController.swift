72f40b4f18ed65bcea27db9d5ac4f35d98a914d7
Hameed Abdullah <hameedabdullah@hameeds-mbp.bmcc.cuny.edu>
2018-11-05 19:06:57 -0500

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Hameed Abdullah on 11/5/18.
+//  Copyright © 2018 Hameed Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
